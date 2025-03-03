1f94998564ac0adb66607956470857a018783730
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-03-03 17:56:35 +0700

    end#19

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,4 +13,11 @@ class ShopController extends Controller
         $products = Product::orderBy('created_at', 'DESC')->paginate(12);
         return view('shop',compact('products'));
     }
+
+    public function product_details($product_slg)
+    {
+        $product = Product::where('slug', $product_slg)->first();
+        $rproducts = Product::where('slug', '<>',$product_slg)->get()->take(8);
+        return view('details',compact('product','rproducts'));
+    }
 }
