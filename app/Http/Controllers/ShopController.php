624385114ac0aa90c8d2ee01a72e8c3a0e22a85a
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-03-09 15:13:59 +0700

    end#24

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
@@ -14,6 +15,7 @@ class ShopController extends Controller
         $o_column = "";
         $o_order = "";
         $order = $request->query('order') ? $request->query('order') : -1;
+        $f_brands = $request->query('brands');
         
         switch($order)
         {
@@ -37,8 +39,12 @@ class ShopController extends Controller
                 $o_column='id';
                 $o_order='ASC';
         }
-        $products = Product::orderBy($o_column,$o_order)->paginate($size);
-        return view('shop',compact('products', 'size', 'order'));
+        $brands = Brand::orderBy('name','ASC')->get();
+        $products = Product::where(function($query) use($f_brands) {
+            $query->whereIn('brand_id',explode(',',$f_brands))->orWhereRaw("'".$f_brands."'=''");
+        })
+                    ->orderBy($o_column,$o_order)->paginate($size);
+        return view('shop',compact('products', 'size', 'order', 'brands', 'f_brands'));
     }
 
     public function product_details($product_slg)
