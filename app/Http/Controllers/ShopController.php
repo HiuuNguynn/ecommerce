df463142122b165cf317157025207646c32faaa9
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-03-09 22:33:58 +0700

    end#25

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use App\Models\Brand;
 use Illuminate\Http\Request;
@@ -16,6 +17,7 @@ class ShopController extends Controller
         $o_order = "";
         $order = $request->query('order') ? $request->query('order') : -1;
         $f_brands = $request->query('brands');
+        $f_categories = $request->query('categories');
         
         switch($order)
         {
@@ -40,11 +42,15 @@ class ShopController extends Controller
                 $o_order='ASC';
         }
         $brands = Brand::orderBy('name','ASC')->get();
+        $categories = Category::orderBy('name','ASC')->get();
         $products = Product::where(function($query) use($f_brands) {
             $query->whereIn('brand_id',explode(',',$f_brands))->orWhereRaw("'".$f_brands."'=''");
         })
+        ->where(function($query) use($f_categories) {
+            $query->whereIn('category_id',explode(',',$f_categories))->orWhereRaw("'".$f_categories."'=''");
+        })
                     ->orderBy($o_column,$o_order)->paginate($size);
-        return view('shop',compact('products', 'size', 'order', 'brands', 'f_brands'));
+        return view('shop',compact('products', 'size', 'order', 'brands', 'f_brands','categories','f_categories'));
     }
 
     public function product_details($product_slg)
