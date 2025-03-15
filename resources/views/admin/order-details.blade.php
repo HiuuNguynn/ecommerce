e51b3987b1d921754ab1db1d14e4492fe39c2cc3
Nguyễn Tuấn Anh <157350887+Tuan2Anh@users.noreply.github.com>
2025-03-15 16:13:58 +0700

    end#43

--- resources/views/admin/order-details.blade.php
+++ resources/views/admin/order-details.blade.php
@@ -27,6 +27,9 @@
                 <a class="tf-button style-1 w208" href="{{route('admin.orders')}}">Back</a>
             </div>
             <div class="table-responsive">
+                @if (Session::has('status'))
+                    <p class="alert alert-success">{{ Session::get('status')}}</p>
+                @endif
                 <table class="table table-striped table-bordered">
                     <tr>
                         <th>Order No</th>
@@ -48,7 +51,7 @@
                         <th>Order Status</th>
                         <td colspan="5">
                             @if($order->status == 'delivered')
-                                <span class="badge badge-success">Delivered</span>
+                                <span class="badge bg-success">Delivered</span>
                             @elseif ($order->status == 'canceled')
                                 <span class="badge bg-danger">Canceled</span>
                             @else
@@ -136,37 +139,26 @@
         </div>
 
         <div class="wg-box mt-5">
-            <h5>Transactions</h5>
-            <table class="table table-striped table-bordered table-transaction">
-                <tbody>
-                    <tr>
-                        <th>Subtotal</th>
-                        <td>{{$order->subtotal}}</td>
-                        <th>Tax</th>
-                        <td>{{$order->tax}}</td>
-                        <th>Discount</th>
-                        <td>{{$order->discount}}</td>
-                    </tr>
-                    <tr>
-                        <th>Total</th>
-                        <td>{{$order->total}}</td>
-                        <th>Payment Mode</th>
-                        <td>{{$transaction->mode}}</td>
-                        <th>Status</th>
-                        <td>
-                            @if($transaction->status == 'approved')
-                                <span class="badge badge-success">Approved</span>
-                            @elseif($transaction->status == 'declinded')
-                                <span class="badge bg-danger">Declinded</span>
-                            @elseif($transaction->status == 'refunded')
-                                <span class="badge bg-secondary">Refunded</span>
-                            @else
-                                <span class="badge bg-warning">Pending</span>
-                            @endif
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <h5>Update Order Status</h5>
+            <form action="{{route('admin.order.status.update')}}" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="hidden" name="order_id" value="{{$order->id}}" />
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="select">
+                        <select name="order_status" id="order_status">
+                            <option value="ordered" {{$order->status == 'ordered' ? "selected" : ""}}>Ordered</option>
+                            <option value="delivered" {{$order->status == 'delivered' ? "selected" : ""}}>Delivered</option>
+                            <option value="canceled" {{$order->status == 'canceled' ? "selected" : ""}}>Canceled</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <button type="submit" class="btn btn-primary tf-button w208">Update Status</button>
+                </div>
+            </div>
+            </form>
         </div>
     </div>
 </div>
